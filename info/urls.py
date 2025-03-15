4dd10dec3bbf373d97a14b47a77410d94f001871
Shivateja Burle <shivateja.idizital@gmail.com>
2025-03-15 12:45:29 +0530

    Login System
    Redirecting to the dashboard (admin, teacher, student) based on the user type.

--- info/urls.py
+++ info/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'info'
 
 urlpatterns = [
+    path('login_success/', views.login_success, name = 'login_success'),
+    path('unauthorized_access/', views.UnauthorizeView.as_view(), name='unauthorize_view'),
     path('', views.IndexView.as_view(), name = 'index_view'),
+    path('admin_home/', views.AdminView.as_view(), name = 'admin_view'),
+    path('teacher/', views.TeacherView.as_view(), name = 'teacher_view'),
+    path('student/', views.StudentView.as_view(), name = 'student_view'),
 ]
--- info/views.py
+++ info/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# Index View
 class IndexView(TemplateView):
     template_name = 'info/index.html'
+
+# Admin View
+class AdminView(LoginRequiredMixin, TemplateView):
+    template_name = 'info/admin/index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            context = self.get_context_data(**kwargs)
+            return self.render_to_response(context)
+        return redirect('info:unauthorize_view')
+
+# Teacher View
+class TeacherView(LoginRequiredMixin, TemplateView):
+    template_name = 'info/teacher/index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_teacher:
+            context = self.get_context_data(**kwargs)
+            return self.render_to_response(context)
+        return redirect('info:unauthorize_view')
+
+# Student View
+class StudentView(LoginRequiredMixin, TemplateView):
+    template_name = 'info/student/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_student:
+            context = self.get_context_data(**kwargs)
+            return self.render_to_response(context)
+        return redirect('info:unauthorize_view')
+    
+# Authorize View
+class UnauthorizeView(TemplateView):
+    template_name = 'info/unauthorize.html'
+
+# Check the user type and redirect to respective dashboard
+def login_success(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('info:admin_view')
+        elif request.user.is_teacher:
+            return redirect('info:teacher_view')
+        elif request.user.is_student:
+            return redirect('info:student_view')
+        else:
+            return render(request, 'info/index.html')
+    return render(request, 'info/index.html')
